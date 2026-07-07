ad3b359ea1f3cf40fe43719f9e38b78a18601308
Icung <icung@Icungs-MacBook-Pro.local>
2026-07-07 21:46:25 +0700

    feat: configure swiftlint

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -7,7 +7,15 @@ import ProjectDescription
 
 let packageSettings = PackageSettings(
     baseSettings: .settings(base: [
-        "LIBTOOLFLAGS": "-no_warning_for_no_symbols"
+        "ASSETCATALOG_COMPILER_GENERATE_SWIFT_ASSET_SYMBOL_EXTENSIONS": "YES",
+        "CLANG_ENABLE_MODULE_VERIFIER": "YES",
+        "ENABLE_MODULE_VERIFIER": "YES",
+        "ENABLE_USER_SCRIPT_SANDBOXING": "YES",
+        "LIBTOOLFLAGS": "-no_warning_for_no_symbols",
+        "LOCALIZATION_PREFERS_STRING_CATALOGS": "YES",
+        "MODULE_VERIFIER_SUPPORTED_LANGUAGES": "objective-c objective-c++",
+        "MODULE_VERIFIER_SUPPORTED_LANGUAGE_STANDARDS": "gnu17 gnu++20",
+        "STRING_CATALOG_GENERATE_SYMBOLS": "YES"
     ])
 )
 #endif
